261f1a69d3d9dcd5675ce4c928bcbd6753b963b4
Hiroshi Miura <miurahr@linux.com>
2026-02-23 12:06:32 +0900

    Update redirection configuration in `astro.config.mjs` and remove `index.astro` redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://iloha-openlab.com',
-    output: 'static'
+    output: 'static',
+    redirects: {
+        '/': '/en/'
+    }
 });
