3398769311d9a9dac03a9c0f92a6031fd65b143f
Hiroshi Miura <miurahr@linux.com>
2026-03-06 08:03:13 +0900

    chote: Disable WebKit and Mobile Safari tests, add new visual regression snapshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,19 +20,19 @@ export default defineConfig({
             name: 'firefox',
             use: { ...devices['Desktop Firefox'] },
         },
-        {
+        /* {
             name: 'webkit',
             use: { ...devices['Desktop Safari'] },
-        },
+        },*/
         /* Test against mobile viewports. */
         {
             name: 'Mobile Chrome',
             use: { ...devices['Pixel 5'] },
         },
-        {
+        /*{
             name: 'Mobile Safari',
             use: { ...devices['iPhone 12'] },
-        },
+        },*/
     ],
     /* Run your local dev server before starting the tests */
     webServer: {
